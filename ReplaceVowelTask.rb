b4df54a22b95539ebcd53c28a7c40ddf8bbcdfa2
Roman Aymaletdinov <Roman.Aymaletdinov@swtecnn.com>
2018-08-03 14:01:28 +0300

    add alg

--- ReplaceVowelTask.rb
+++ ReplaceVowelTask.rb
@@ -5,10 +5,14 @@ class ChangeVowel
   public def initialize
     welcome_message
     user_string = gets.chomp
-    puts "Test 1/2..."
-    replace_0(user_string)
-   # puts "Test 2/2..."
-   # replace_2(superStringTest)
+    puts "Test 1/4..."
+    replace_1(user_string)
+    puts "Test 2/4..."
+    replace_2(user_string)
+    puts "Test 3/4..."
+    replace_3(user_string)
+    puts "Test 4/4"
+    replace_4(user_string)
   end
 
   private def superStringTest
@@ -29,57 +33,72 @@ Please, enter u line below: \n"
 #
 #   //////////////////////////////////////////////////////////////////////////
 #
-  private def replace_0(user_string = "qwerty")
+  private def replace_4(user_string = "qwerty")
+    startTime = Time.new
+    stop = user_string.length
+
+    for i in 0...stop
+      break if i == stop
+
+      if user_string[i] =~ /[EYUIOAeyuioa]/
+        stop.downto(i+1) do |k|
+          if user_string[k] =~ /[EYUIOAeyuioa]/
+            tmp = user_string[i]
+            user_string[i] = user_string[k]
+            user_string[k] = tmp
+
+            stop = k - 1
+            break
+          end
+        end
+      end
+    end
+
+    endTime = Time.new
+    puts "...\nThank's! Your reverse line: #{user_string}"
+  #  puts "Finish! Current speed: #{endTime.to_i - startTime.to_i}"
+  end
+#
+#   //////////////////////////////////////////////////////////////////////////
+#
+  private def replace_3(user_string = "qwerty")
     startTime = Time.new
 
     cursor_1 = 0
     cursor_2 = user_string.length - 1
-    result = -1
+    result   = -1
 
+    char_1 = -1
     loop do
-      break if (cursor_2 - cursor_1) <= 0
-
       result = my_for(cursor_1, cursor_2, user_string)
 
-        puts "cursor_1: #{cursor_1}"
-        puts "cursor_2: #{cursor_2}"
-        puts "result: #{result} \n ------------- "
-
       if result != -1
+        char_1 = result
+
         cursor_1 = cursor_2
-        cursor_2 = result +1
+        cursor_2 = result + 1
         result = my_negative_for(cursor_1, cursor_2, user_string)
 
-        puts "cursor_1: #{cursor_1}"
-        puts "cursor_2: #{cursor_2}"
-        puts "result: #{result}"
-
         if result != -1
+          tmp = user_string[char_1]
+          user_string[char_1] = user_string[result]
+          user_string[result] = tmp
+
           cursor_1 = cursor_2
-          cursor_2 = result -1
-
-          tmp = user_string[cursor_1]
-          user_string[cursor_1] = user_string[cursor_2]
-          user_string[cursor_2] = tmp
-
-          cursor_1 += 1
-          cursor_2
+          cursor_2 = result - 1
         else break
         end
       else break
       end
-      puts user_string
-      sleep(1)
     end
       endTime = Time.new
       puts "...\nThank's! Your reverse line: #{user_string}"
-      puts "Finish! Current speed: #{endTime.to_i - startTime.to_i}"
+   #   puts "Finish! Current speed: #{endTime.to_i - startTime.to_i}"
   end
 
 
   private def my_for(start, stop, str)
     for i in start...stop
-      puts "str[i]: #{str[i]} (i = #{i}) str = #{str}"
       return i if str[i] =~ /[EYUIOAeyuioa]/
     end
     return -1
@@ -87,10 +106,7 @@ Please, enter u line below: \n"
 
   private def my_negative_for(start, stop, str)
     (start).downto(stop) do |i|
-      puts "str[i]: #{str[i]} (i = #{i}) str = #{str}"
-      if str[i] =~ /[EYUIOAeyuioa]/
-        return i
-      end
+      return i if str[i] =~ /[EYUIOAeyuioa]/
     end
     return -1
   end
@@ -99,7 +115,7 @@ Please, enter u line below: \n"
 #   faster by 60%
 #   //////////////////////////////////////////////////////////////////////////
 #
-  private def replace_1(user_string = "qwerty")
+  private def replace_2(user_string = "qwerty")
     startTime = Time.new
     indexes = Array.new()
     chars = Array.new()
@@ -120,14 +136,14 @@ Please, enter u line below: \n"
     end
 
     endTime = Time.new
-  #  puts "...\nThank's! Your reverse line: #{user_string}"
-    puts "Finish! Current speed: #{endTime.to_i - startTime.to_i}"
+    puts "...\nThank's! Your reverse line: #{user_string}"
+   # puts "Finish! Current speed: #{endTime.to_i - startTime.to_i}"
   end
 
 #    very slow alghorithm
 #   //////////////////////////////////////////////////////////////////////////
 #
-  private def replace_2(user_string = "qwerty")
+  private def replace_1(user_string = "qwerty")
     startTime = Time.new
 
     vowels = user_string.gsub(/[^EYUIOAeyuioa]/, "")
@@ -141,8 +157,8 @@ Please, enter u line below: \n"
     end
 
     endTime = Time.new
-  #  puts "...\nThank's! Your reverse line: #{user_string}"
-    puts "Finish! Current speed: #{endTime.to_i - startTime.to_i}"
+    puts "...\nThank's! Your reverse line: #{user_string}"
+   # puts "Finish! Current speed: #{endTime.to_i - startTime.to_i}"
   end
 
 #
